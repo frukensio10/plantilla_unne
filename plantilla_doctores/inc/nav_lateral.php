a0d253702d564f040748b0f8418d35e2f19d3679
eduardo <38326488+frukensio10@users.noreply.github.com>
2021-05-29 12:08:28 -0300

    unir carpetas
    hacer MVC con php

---
+++ plantilla_doctores/inc/nav_lateral.php
@@ -0,0 +1,122 @@
+<section class="full-box nav-lateral">
+			<div class="full-box nav-lateral-bg show-nav-lateral"></div>
+			<div class="full-box nav-lateral-content">
+				<figure class="full-box nav-lateral-avatar">
+					<i class="far fa-times-circle show-nav-lateral"></i>
+					<img src="vistas/assets/avatar/Avatar.png" class="img-fluid" alt="Avatar">
+					<figcaption class="roboto-medium text-center">
+						Carlos Alfaro <br><small class="roboto-condensed-light">Web Developer</small>
+					</figcaption>
+				</figure>
+				<div class="full-box nav-lateral-bar"></div>
+				<nav class="full-box nav-lateral-menu">
+					<ul>
+						<li>
+							<!--<a href="home.html"><i class="fab fa-dashcube fa-fw"></i> &nbsp; Inicio</a>-->
+						</li>
+
+						<li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-users fa-fw"></i> &nbsp; Alumnos <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="nuevos_alumnos.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Agregar alumno</a>
+								</li>
+								
+								<li>
+									<a href="buscar_alumno.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar alumnno</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-store-alt fa-fw"></i> &nbsp; Facultades <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="facultad_nueva.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Agregar facultad</a>
+								</li>
+								
+								<li>
+									<a href="buscar_facultad.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar facultad</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-store-alt fa-fw"></i> &nbsp; Carreras <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="nueva_carrera.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Agregar carrera</a>
+								</li>
+								
+								<li>
+									<a href="buscar_carrera.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar carrera</a>
+								</li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-capsules"></i> &nbsp; Medicamentos <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="nuevo_medicamento.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Agregar medicamento</a>
+								</li>
+								
+								<li>
+									<a href="buscar_medicamento.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar medicamentos</a>
+								</li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#" class="nav-btn-submenu"><i class="far fa-address-book"></i> &nbsp; Citas <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="nueva_reservacion.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Nueva cita</a>
+								</li>
+								<li>
+									<a href="reservaciones.html"><i class="far fa-calendar-alt fa-fw"></i> &nbsp; Reservaciones</a>
+								</li>
+								
+								<li>
+									<a href="lista_reservaciones.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Finalizados</a>
+								</li>
+								<li>
+									<a href="buscar_citas.html"><i class="fas fa-search-dollar fa-fw"></i> &nbsp; Buscar por fecha</a>
+								</li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#" class="nav-btn-submenu"><i class="fas  fa-user-secret fa-fw"></i> &nbsp; Usuarios <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="nuevo_usuario.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Nuevo usuario</a>
+								</li>
+								
+								<li>
+									<a href="buscar_usuario.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar usuario</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-notes-medical"></i> &nbsp; Consulta medica <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="nueva_consultaMEDICA.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Nueva consulta</a>
+								</li>
+								
+								<li>
+									<a href="historiales_de_los_alumnos.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar historia clinica</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="especialidades.html"><i class="fa fa-user-md"></i> &nbsp; Especialidades</a>
+						</li>
+						
+
+						<li>
+							<a href="unne.html"><i class="fas fa-store-alt fa-fw"></i> &nbsp; UNNE</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</section>
